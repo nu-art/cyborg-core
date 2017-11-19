1f3757809e0dc2b73728a78ac6579cb9bffb34c0
TacB0sS <adam.zehavi@nu-art-software.com>
2017-11-20 00:59:10 +0200

    reverted

--- src/main/java/com/nu/art/cyborg/core/CyborgActivityBridgeImpl.java
+++ src/main/java/com/nu/art/cyborg/core/CyborgActivityBridgeImpl.java
@@ -42,7 +42,6 @@ import com.nu.art.core.tools.ArrayTools;
 import com.nu.art.cyborg.core.CyborgBuilder.LaunchConfiguration;
 import com.nu.art.cyborg.core.abs.Cyborg;
 import com.nu.art.cyborg.core.abs._SystemServices;
-import com.nu.art.cyborg.core.consts.DebugFlags;
 import com.nu.art.cyborg.core.consts.IntentKeys;
 import com.nu.art.cyborg.core.consts.LifeCycleState;
 import com.nu.art.cyborg.core.interfaces.LifeCycleListener;
@@ -510,8 +509,9 @@ public class CyborgActivityBridgeImpl
 	}
 
 	@SuppressWarnings("unchecked")
-	public final <ListenerType> void dispatchEvent(String message, final Class<ListenerType> listenerType, final Processor<ListenerType> processor) {
+	public final <ListenerType> void dispatchEvent(String message, final Class<ListenerType> eventType, final Processor<ListenerType> processor) {
 		logDebug("Dispatching UI Event: " + message);
+
 		final WhoCalledThis whoCalledThis = new WhoCalledThis("Dispatching UI Event: " + message);
 		activity.runOnUiThread(new Runnable() {
 
@@ -520,8 +520,7 @@ public class CyborgActivityBridgeImpl
 				if (isDestroyed() || isSavedState())
 					return;
 
-				Class<ListenerType> _listenerType = DebugFlags.paramExtractor.extractGenericTypeFromProcessorTest(listenerType, processor);
-				eventDispatcher.dispatchEvent(whoCalledThis, _listenerType, processor);
+				eventDispatcher.dispatchEvent(whoCalledThis, eventType, processor);
 			}
 		});
 	}
--- src/main/java/com/nu/art/cyborg/core/consts/DebugFlags.java
+++ src/main/java/com/nu/art/cyborg/core/consts/DebugFlags.java
@@ -1,12 +1,5 @@
 package com.nu.art.cyborg.core.consts;
 
-import com.nu.art.belog.Logger;
-import com.nu.art.core.generics.Processor;
-
-import java.lang.reflect.Array;
-import java.lang.reflect.Field;
-import java.lang.reflect.Type;
-
 /**
  * Created by tacb0ss on 14/11/2017.
  */
@@ -16,39 +9,4 @@ public class DebugFlags {
 	public static boolean DebugPerformance = false;
 	public static boolean DebugActivityLifeCycle = false;
 	public static boolean DebugControllerLifeCycle = false;
-	public static ExtractGenericParamFromProcessor paramExtractor;
-
-	public static class ExtractGenericParamFromProcessor
-			extends Logger {
-
-		private Field argsField;
-
-		private Field resolvedTypesField;
-
-		public void initGenericParamExtractionFromProcessorTesting() {
-			try {
-				Class<?> parameterizedType = Class.forName("libcore.reflect.ParameterizedTypeImpl");
-				argsField = parameterizedType.getDeclaredField("args");
-				argsField.setAccessible(true);
-
-				Class<?> listOfTypes = Class.forName("libcore.reflect.ListOfTypes");
-				resolvedTypesField = listOfTypes.getDeclaredField("resolvedTypes");
-				resolvedTypesField.setAccessible(true);
-			} catch (Throwable e) {
-				logWarning("Error while setting up the generic params extraction test", e);
-			}
-		}
-
-		@SuppressWarnings("unchecked")
-		public <T> Class<T> extractGenericTypeFromProcessorTest(Class<T> fallback, Processor<T> processor) {
-			try {
-				Type type = processor.getClass().getGenericInterfaces()[0];
-				Object list;
-				list = argsField.get(type);
-				return (Class<T>) Array.get(resolvedTypesField.get(list), 0);
-			} catch (Throwable e) {
-				return fallback;
-			}
-		}
-	}
 }
