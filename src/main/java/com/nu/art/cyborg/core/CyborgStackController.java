7bff8a1026dc7559e3c28384e0ec66b361ffca1a
TacB0sS <adam.zehavi@nu-art-software.com>
2017-11-29 00:45:25 +0200

    disabled the isAnimating early exit, and allowed the next stack layer builder to push or pop before previous transition has end.

--- src/main/java/com/nu/art/cyborg/core/CyborgStackController.java
+++ src/main/java/com/nu/art/cyborg/core/CyborgStackController.java
@@ -471,10 +471,10 @@ public final class CyborgStackController
 	}
 
 	private void push(final StackLayer targetLayerToBeAdded) {
-		if (animatingTransition) {
-			logWarning("NOT PUSHING NEW LAYER... TRANSITION ANIMATION IN PROGRESS!!!");
-			return;
-		}
+//		if (animatingTransition) {
+//			logWarning("NOT PUSHING NEW LAYER... TRANSITION ANIMATION IN PROGRESS!!!");
+//			return;
+//		}
 
 		// promote this stack in the hierarchy, so it will receive the events first.
 		// SHOULD I MANAGE A STACK OF STACKS, THAT WILL DEFINE THE ORDER OF WHICH EVENTS ARE RECEIVED, ACCORDING TO PUSH RECENCY...?
@@ -546,10 +546,10 @@ public final class CyborgStackController
 	}
 
 	public boolean popLast() {
-		if (animatingTransition) {
-			logWarning("NOT POPPING LAST LAYER... TRANSITION ANIMATION IN PROGRESS!!!");
-			return true;
-		}
+//		if (animatingTransition) {
+//			logWarning("NOT POPPING LAST LAYER... TRANSITION ANIMATION IN PROGRESS!!!");
+//			return true;
+//		}
 
 		final StackLayer targetLayerToBeRemove = getAndRemoveTopLayer();
 		if (targetLayerToBeRemove == null)
