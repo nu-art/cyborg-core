cb2b6a6b3ab893f408c8a23197132eb5eb7cfa44
TacB0sS <adam.zehavi@nu-art-software.com>
2018-05-16 11:50:16 +0300

    exception log improved

--- src/main/java/com/nu/art/cyborg/core/modules/AndroidLogClient.java
+++ src/main/java/com/nu/art/cyborg/core/modules/AndroidLogClient.java
@@ -20,7 +20,12 @@ package com.nu.art.cyborg.core.modules;
 
 import com.nu.art.belog.BeLoggedClient;
 import com.nu.art.belog.consts.LogLevel;
+import com.nu.art.core.interfaces.Getter;
 import com.nu.art.core.tools.ExceptionTools;
+import com.nu.art.core.utils.SynchronizedObject;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by TacB0sS on 28-Feb 2017.
@@ -29,31 +34,58 @@ public class AndroidLogClient
 	extends BeLoggedClient {
 
 	private static final String StacktraceIndent = "    ";
-	private final StringBuffer buffer = new StringBuffer();
+	private SynchronizedObject<StringBuffer> syncBuffer = new SynchronizedObject<>(new Getter<StringBuffer>() {
+		@Override
+		public StringBuffer get() {
+			return new StringBuffer();
+		}
+	});
+	private SynchronizedObject<HashSet<StackTraceElement>> syncTracesSet = new SynchronizedObject<>(new Getter<HashSet<StackTraceElement>>() {
+		@Override
+		public HashSet<StackTraceElement> get() {
+			return new HashSet<>();
+		}
+	});
 
 	@Override
 	protected void log(LogLevel level, String thread, String tag, String message, Throwable t) {
 		if (!isLoggable(level))
 			return;
 
-		String tagWithThread;
-		synchronized (buffer) {
-			buffer.append(thread).append("/").append(tag);
-			tagWithThread = buffer.toString();
-			buffer.setLength(0);
-		}
+		StringBuffer buffer = syncBuffer.get();
+		buffer.setLength(0);
+		buffer.append(thread).append("/").append(tag);
+
+		String tagWithThread = buffer.toString();
 
 		printLog(level, tagWithThread, message);
 
+		if (t != null)
+			logException(level, t, tagWithThread);
+	}
+
+	private void logException(LogLevel level, Throwable t, String tagWithThread) {
 		boolean isCause = false;
+		Set<StackTraceElement> traces = syncTracesSet.get();
+
 		while (t != null) {
 			String exceptionMessage = t.getMessage() != null ? ": " + t.getMessage() : "";
 			//noinspection StringEquality
 			String causedBy = isCause ? "CAUSED BY: " : "";
 
 			printLog(level, tagWithThread, causedBy + t.getClass().getName() + exceptionMessage);
-			for (StackTraceElement stackTraceElement : t.getStackTrace()) {
+			StackTraceElement[] stackTrace = t.getStackTrace();
+
+			for (int i = 0; i < stackTrace.length; i++) {
+				StackTraceElement stackTraceElement = stackTrace[i];
 				printLog(level, tagWithThread, StacktraceIndent + ExceptionTools.parseStackTrace(stackTraceElement));
+
+				if (traces.contains(stackTraceElement)) {
+					printLog(level, tagWithThread, StacktraceIndent + "... " + (stackTrace.length - i) + " more duplicate traces ");
+					break;
+				}
+
+				traces.add(stackTraceElement);
 			}
 
 			isCause = true;
