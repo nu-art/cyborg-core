f7f80307316e792287556edc19ff9a5c4d190c00
TacB0sS <adam.vdk@nu-art-software.com>
2019-01-28 23:02:10 +0200

    component testing

--- src/test/java/com/nu/art/modules/cache/Test_CacheModule.java
+++ src/test/java/com/nu/art/modules/cache/Test_CacheModule.java
@@ -7,24 +7,24 @@ public class Test_CacheModule
 
 	@Test
 	public void test_CacheOne() {
-		AsyncScenario testGroup = createAsyncScenario();
-		testGroup.addTest(cacheUrlSuccess("JazzIcon", "png", URL_musicIconCategoryJazz));
-		testGroup.execute();
+		createAsyncScenario()
+			.addTest(cacheUrlSuccess("JazzIcon", "png", URL_musicIconCategoryClassical))
+			.execute();
 	}
 
 	@Test
 	public void test_CacheTwoTheDifferent() {
-		AsyncScenario testGroup = createAsyncScenario();
-		testGroup.addTest(cacheUrlSuccess("JazzIcon", "png", URL_musicIconCategoryJazz));
-		testGroup.addTest(cacheUrlSuccess("JazzIcon", "png", URL_musicIconCategoryClassical));
-		testGroup.execute();
+		createAsyncScenario()
+			.addTest(cacheUrlSuccess("JazzIcon", "png", URL_musicIconCategoryJazz))
+			.addTest(cacheUrlSuccess("ClassicalIcon", "png", URL_musicIconCategoryClassical))
+			.execute();
 	}
 
 	@Test
 	public void test_CacheTwoTheSame() {
-		AsyncScenario testGroup = createAsyncScenario();
-		testGroup.addTest(cacheUrlSuccess("JazzIcon-1", "png", URL_musicIconCategoryJazz));
-		testGroup.addTest(cacheUrlSuccess("JazzIcon-2", "png", URL_musicIconCategoryJazz));
-		testGroup.execute();
+		createAsyncScenario()
+			.addTest(cacheUrlSuccess("JazzIcon-1", "png", URL_musicIconCategoryJazz))
+			.addTest(cacheUrlSuccess("JazzIcon-2", "png", URL_musicIconCategoryJazz))
+			.execute();
 	}
 }
